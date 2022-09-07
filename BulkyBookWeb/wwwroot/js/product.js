9be8a4bc1b342ceb877e6a4b374cb36837ddce51
JonhSnap <jonhsnap123@gmail.com>
2022-09-07 15:43:05 +0700

    Product Get Action UI Projections using Select View Models 1.Sweetalerts 2.Create First Product 3.Add API EndPoint for DataTable 4.Loading Datatables

---
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -0,0 +1,49 @@
+﻿//var dataTable;
+
+//$(document).ready(function () {
+//    loadDataTable();
+//});
+
+//function loadDataTable() {
+//    $(document).ready(function () {
+//        let obj = {
+//            data: data,
+//            columns: [
+//                { data: 'title' },
+//                { data: 'isbn' },
+//                { data: 'price' }
+//            ]
+//        }
+//        $('#tblData').DataTable({
+//            "ajax": {
+//                "url": "/admin/product/getall"
+//            },
+//            'columns': [
+//                { data: 'title', "width": "15%" },
+//                { data: 'isbn', "width": "15%" },
+//                { data: 'price', "width": "15%" }
+//            ]
+//        });
+//    });
+//}
+
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            { "data": "author", "width": "15%" },
+            { "data": "category", "width": "15%" }
+        ]
+    });
+}
